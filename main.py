dd4d0d67861d4cac522cd02a469357b1e4613cd8
Габербуш Денис <tvoikoteika228@gmail.com>
2019-04-03 03:11:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ while 1:
     try:
         a = api.wall.getComments(owner_id='-174782021', post_id='881', sort='desc', count='1', preview_length='1', v='5.92')
         if time.time()-a['items'][0]['date'] > 300 and a['items'][0]['from_id'] != api.users.get(v=5.92)[0]['id']:
-            api.wall.createComment(owner_id='-174782021', post_id='881', message=answers[random.randint(0, len(answers)-1)])
+            api.wall.createComment(owner_id='-174782021', post_id='881', 
+                                   message=answers[random.randint(0, len(answers)-1)], v='5.92')
         time.sleep(5)
     except:
         continue
